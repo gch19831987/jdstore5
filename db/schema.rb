6c465679d8307cc570faa9181383c7e075d161f4
zqz <71264737@qq.com>
2017-03-01 23:05:58 +0800

    rake db three brothers

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,7 @@ ActiveRecord::Schema.define(version: 20170219003231) do
     t.string   "title"
     t.text     "description"
     t.integer  "quantity"
-    t.integer  "price"
+    t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "image"
