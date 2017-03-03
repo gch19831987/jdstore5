a6b74928139d3bc3384908d28a198037008c3f8c
zqz <71264737@qq.com>
2017-03-03 14:37:12 +0800

    add user_id to group

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :orders  
   has_many :reviews
+  has_many :groups
   def admin?
 	is_admin
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170303060308) do
+ActiveRecord::Schema.define(version: 20170303063346) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170303060308) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "orders", force: :cascade do |t|
